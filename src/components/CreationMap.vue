db807a85b6075e0c45cd1a21d14b65decc16fb7d
RaveTheN <peppereres@live.it>
2024-05-23 12:34:08 +0200

    /add responsive latlng in dialog

--- src/components/CreationMap.vue
+++ src/components/CreationMap.vue
@@ -1,5 +1,23 @@
 <template>
-  <div id="blank"></div>
+  <v-card color="surface-light">
+    <div id="blank"></div>
+    <v-text-field
+      id="latitude"
+      v-model="latitude"
+      label="Latitude"
+      variant="outlined"
+      density="compact"
+      max-width="30%"
+    ></v-text-field>
+    <v-text-field
+      id="longitude"
+      v-model="longitude"
+      label="Longitude"
+      variant="outlined"
+      density="compact"
+      max-width="30%"
+    ></v-text-field>
+  </v-card>
 </template>
 <script>
 import L from "leaflet";
@@ -9,6 +27,9 @@ export default {
     return {
       map: L.Map,
       marker: L.Marker,
+      markerLayer: L.LayerGroup,
+      latitude: "",
+      longitude: "",
     };
   },
 
@@ -23,15 +44,28 @@ export default {
         document.getElementById("blank").setAttribute("id", "map");
       }
       this.map = L.map("map").setView([51.505, -0.09], 13);
+      this.markerLayer = new L.LayerGroup();
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(this.map);
       this.map.on("click", this.onMapClick);
+      this.markerLayer.addTo(this.map);
     },
     onMapClick(e) {
-      console.log("You clicked the map at " + e.latlng);
-      this.marker = L.marker(e.latlng).addTo(this.map);
+      if (this.markerLayer.getLayers().length >= 1) {
+        this.markerLayer.clearLayers();
+
+        this.getLatLng(e);
+      } else {
+        this.getLatLng(e);
+      }
+    },
+
+    getLatLng(e) {
+      this.marker = L.marker(e.latlng).addTo(this.markerLayer);
+      this.latitude = e.latlng.lat;
+      this.longitude = e.latlng.lng;
     },
   },
 
